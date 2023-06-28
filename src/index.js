36be93c4651e17d58cc75b317ef83ffcc32dff90
anik588 <sajjad.anik24@gmail.com>
2023-06-29 00:26:37 +0600

    Update code and index.html

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+//import App from './App';
+import Product from './components/Product.js';
 
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<App />);
+root.render(<Product />);
